4acf9bee2912aac5e5b10c687a81c4e6935dbde6
tomash-tech <tomashavran.pracovni@gmail.com>
2026-04-14 15:47:07 +0200

    změna verze gemini

--- api/chat.js
+++ api/chat.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
       .join('\n');
 
 const response = await fetch(
-  `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-002:generateContent`,
+  `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro:generateContent`,
   {
     method: 'POST',
     headers: {
