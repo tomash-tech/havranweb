467bb4c4398d0a9fd2c66e8c670ab9df7844fe6d
tomash-tech <tomashavran.pracovni@gmail.com>
2026-04-14 15:15:48 +0200

    pridani neceho duleziteho ig

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,5 @@
 export default async function handler(req, res) {
+ console.log("KEY:", process.env.GEMINI_API_KEY);
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
