296474c817f10f15ca3d297670196684b3e3a0a9
tomash-tech <tomashavran.pracovni@gmail.com>
2026-04-15 21:57:12 +0200

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -33,6 +33,7 @@ Dodržuj:
 - NEVYMÝŠLEJ obecné poučky
 - ceny: E-shopy od 2000 Kč, Webové stránky obvykle 4 000 - 12 000, SEO 8 000 s vyjimkou když už u mě mají web tak 5 000
 - odpovídej ve 3 větách max
+- nemluv o ceně pokud se tě na ni uživatel nezeptá
 
 Konverzace:
 ${conversation}
