c5ffbcf22c687a9a3ed55e993e52642f05cbae1f
tomash-tech <tomashavran.pracovni@gmail.com>
2026-05-05 20:06:26 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,6 +16,7 @@ export default function Header() {
           <Link to="/legal" className="text-gray-300 hover:text-white transition-colors font-medium">Legální</Link>
           <Link to="/blog" className="text-gray-300 hover:text-white transition-colors font-medium">Blog</Link>
           <Link to="/kontakt" className="text-gray-300 hover:text-white transition-colors font-medium">Kontakt</Link>
+          <Link to="/cenik" className="text-gray-300 hover:text-white transition-colors font-medium">Ceník</Link>
         </div>
       </div>
     </nav>
