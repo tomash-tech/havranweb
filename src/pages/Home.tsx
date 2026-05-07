1e1d818e51d66e8de54f65543a9657c91f9d3187
tomash-tech <tomashavran.pracovni@gmail.com>
2026-05-07 15:15:47 +0200

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,6 +7,7 @@ import { blogPosts } from '../lib/blogPosts';
 import { useEffect } from 'react';
 
 function Home() {
+  useEffect(() => {
   let canonical = document.querySelector('link[rel="canonical"]');
   if (!canonical) { canonical = document.createElement('link'); canonical.setAttribute('rel', 'canonical'); document.head.appendChild(canonical); }
   canonical.setAttribute('href', 'https://havranweb.cz/');
